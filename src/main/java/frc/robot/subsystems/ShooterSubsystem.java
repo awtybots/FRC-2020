d26083a45ba2506592be924aef2f09687bf673c6
matt-mekha <51768075+matt-mekha@users.noreply.github.com>
2020-01-08 17:35:14 -0600

    added empty shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    
+    public ShooterSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+        
+    }
+}
