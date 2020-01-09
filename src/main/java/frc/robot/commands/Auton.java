303383992011052a49fb9702daaa44be1ae20112
matt-mekha <51768075+matt-mekha@users.noreply.github.com>
2020-01-09 16:40:04 -0600

    forgot to package classes

--- src/main/java/frc/robot/commands/Auton.java
+++ src/main/java/frc/robot/commands/Auton.java
@@ -1,4 +1,5 @@
-import edu.wpi.first.wpilibj.XboxController;
+package frc.robot.commands;
+
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
--- src/main/java/frc/robot/commands/Teleop.java
+++ src/main/java/frc/robot/commands/Teleop.java
@@ -1,3 +1,5 @@
+package frc.robot.commands;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.DriveTrainSubsystem;
