bfef8778f4e12a49f15f6decb8ed04ef7d1f44fb
Awtybots Laptop <awtyrobotics@gmail.com>
2020-03-06 16:45:42 -0600

    changed CAN IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,25 +18,25 @@ public final class Constants {
 
     public final static class MotorIDs {
         public static final int DRIVE_L1 = 1;
-        public static final int DRIVE_L2 = 9;
+        public static final int DRIVE_L2 = 2;
 
-        public static final int DRIVE_R1 = 10;
+        public static final int DRIVE_R1 = 3;
         public static final int DRIVE_R2 = 4;
 
-        public static final int INTAKE = 3;
+        public static final int INTAKE = 9;
 
         public static final int INDEXER_L = 8;
-        public static final int INDEXER_R = 2;
+        public static final int INDEXER_R = 7;
 
-        public static final int TOWER_1 = 7;
+        public static final int TOWER_1 = 5;
         public static final int TOWER_2 = 6;
 
-        public static final int FLYWHEEL = 16;
+        public static final int FLYWHEEL = 10;
 
         public static final int CLIMB_1 = 11;
         public static final int CLIMB_2 = 12;
 
-        public static final int CONTROL_PANEL_SPINNER = 5;
+        public static final int CONTROL_PANEL_SPINNER = 13;
     }
 
     public final static class SolenoidChannels {
