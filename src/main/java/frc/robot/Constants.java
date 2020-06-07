94181c1961bc75467f54b0c7b6e89fce1fa809a2
AlexDvorak <doe.jimmie@gmail.com>
2020-06-07 17:33:14 -0500

    better comments + naming + styling

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,16 +4,18 @@ import frc.robot.commands.drive.TeleopDrive.DriveControls;
 import frc.robot.subsystems.DrivetrainSubsystem.DriveMode;
 import frc.robot.subsystems.DrivetrainSubsystem.MotorControlMode;
 
-public final class Constants {
+public final class Constants
+{
 
     public static final double PERIOD = 0.02;
 
-    public final static class DriveTrain {
+    public final static class DriveTrain
+    {
         // OPTIONS
-        public final static boolean IS_TUNING = false; // whether or not to tune PID/FeedForward on SmartDashboard
-        public final static MotorControlMode MOTOR_CONTROL_MODE = MotorControlMode.PID; // How the motors are controlled to reach target velocity
-        public final static DriveMode DRIVE_MODE                = DriveMode.RAMPED_PERCENT; // the response curve of the motors
-        public final static DriveControls DRIVE_CONTROLS        = DriveControls.ARCADE_DRIVE; // how the sticks are used to determine motor speed
+        public final static boolean IS_TUNING = false; // enable PID/FeedForward tuning on SmartDashboard
+        public final static MotorControlMode MOTOR_CONTROL_MODE = MotorControlMode.PID;       // target velocity --> motor output
+        public final static DriveMode DRIVE_MODE                = DriveMode.RAMPED_PERCENT;   // acceleration curve of motors
+        public final static DriveControls DRIVE_CONTROLS        = DriveControls.ARCADE_DRIVE; // stick input --> target veocity
 
         // AUTON
         public final static double MAX_OUTPUT_AUTON = 0.5;
@@ -23,11 +25,12 @@ public final class Constants {
         // VALUES
         public final static double WHEEL_CIRCUMFERENCE = 6.375 * Math.PI;
         public final static double TRACK_WIDTH         = 26.5; // inches between left wheels and right wheels
-        public final static double GEAR_RATIO          = 12.0 / 40.0 * 24.0 / 34.0;
+        public final static double GEAR_RATIO          = (12.0 / 40.0) * (24.0 / 34.0);
     }
 
-    public final static class Shooter {
-        // Enable PID tuning TODO this should be tuned and removed before comp
+    public final static class Shooter
+    {
+        // Enable PID tuning
         public final static boolean IS_TUNING = false;
 
         // Manual Shooting speeds
@@ -41,13 +44,15 @@ public final class Constants {
         public final static double TARGET_ANGLE_SLOW_THRESHOLD = 15; // degrees
     }
 
-    public final static class Limelight {
-        public final static double CAMERA_HORIZONTAL_SKEW = 1.75;
-        public final static double CAMERA_MOUNTING_ANGLE = 41;
+    public final static class Limelight
+    {
+        public final static double HORIZONTAL_SKEW = 1.75;
+        public final static double MOUNTING_ANGLE = 41;
         public final static double BOOT_TIME = 45; // seconds
     }
 
-    public final static class ControlPanelSpinner {
+    public final static class ControlPanelSpinner
+    {
         public final static double COLOR_PASSES = 7;
         public final static double MOTOR_SPEED = 0.3;
 
@@ -56,7 +61,7 @@ public final class Constants {
         public final static double[] RED     = new double[]{.52, .34, .13};
         public final static double[] YELLOW  = new double[]{.31, .55, .12};
 
-        public final static double VERIFY_COLOR_SEC = 0.03; // time (seconds) to verify color is really there (0.125 is 60 RPM)
+        public final static double VERIFY_COLOR_SEC = 0.03; // wait time to verify color (0.125s is 60 RPM)
         public final static double MIN_COLOR_CONFIDENCE = 0.95;
     }
 }
